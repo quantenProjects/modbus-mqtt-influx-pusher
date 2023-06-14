64312225db842ef185b911b7828113141d196af1
quantenProjects <quantenProjects@users.noreply.github.com>
2023-06-14 18:51:57 +0200

    pusher fix mqtt

--- mqip/pusher/mqtt.py
+++ mqip/pusher/mqtt.py
@@ -21,19 +21,19 @@ class MqttPusher(Pusher):
         super().__init__()
 
     def connect(self):
-        mqtt_client = mqtt.Client()
+        self.client = mqtt.Client()
         if "ca_cert" in self.config["mqtt"]:
-            mqtt_client.tls_set(ca_certs=self.config["mqtt"]["ca_cert"], certfile=self.config["mqtt"]["cert"], keyfile=self.config["mqtt"]["key"])
+            self.client.tls_set(ca_certs=self.config["mqtt"]["ca_cert"], certfile=self.config["mqtt"]["cert"], keyfile=self.config["mqtt"]["key"])
         if "username" in self.config["mqtt"]:
-            mqtt_client.username_pw_set(self.config["mqtt"]["username"], self.config["mqtt"]["password"])
-        mqtt_client.connect(self.config["mqtt"]["host"], self.config["mqtt"].getint("port"))
-        mqtt_client.loop_start()
+            self.client.username_pw_set(self.config["mqtt"]["username"], self.config["mqtt"]["password"])
+        self.client.connect(self.config["mqtt"]["host"], self.config["mqtt"].getint("port"))
+        self.client.loop_start()
 
     def push(self, measurement_name: str, data: dict, mapped_data: dict, timestamp: Optional[datetime.datetime] = None):
         for value_name in data:
             self.client.publish(self.topic_prefix + "/" + measurement_name + "/" + value_name, data[value_name])
         for value_name in mapped_data:
-            self.client.publish(self.topic_prefix + "/" + measurement_name + "/" + value_name, data[value_name])
+            self.client.publish(self.topic_prefix + "/" + measurement_name + "/" + value_name + "/mapped", mapped_data[value_name])
 
     def flush(self):
         pass
